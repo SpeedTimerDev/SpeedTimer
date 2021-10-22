a1ede27810d1d452a13eecd9bee1378864fbff49
CoderGuruXYZ <72657147+CoderGuruXYZ@users.noreply.github.com>
2021-10-22 14:50:10 +0100

    beta v0.1.4

---
+++ cubeslistDb4x4.js
@@ -0,0 +1,167 @@
+var cubesFour = [
+    {
+        Name: "YJ MGC",
+        Weight: "115g",
+        Size: "60mm",
+        Stickerless: "Yes",
+        Magnetic: "Yes",
+        Price: "$19.99",
+        Rating: "9/10",
+        Link: "https://speedcubing.org/collections/4x4x4/products/yj-mgc-4x4x4?variant=32279439900750",
+        Img: "cubeslist/yjmgc4.jpeg"
+    },
+    {
+        Name: "GAN 460",
+        Weight: "139.5g",
+        Size: "60mm",
+        Stickerless: "Yes",
+        Magnetic: "Yes",
+        Price: "$55.99",
+        Rating: "6/10",
+        Link: "none",
+        Img: "cubeslist/gan460.jpeg"
+    },
+    {
+        Name: "MoYu AoSu GTS2 M",
+        Weight: "122g",
+        Size: "61mm",
+        Stickerless: "Yes",
+        Magnetic: "Yes",
+        Price: "$29.99",
+        Rating: "8/10",
+        Link: "https://speedcubing.org/collections/4x4x4/products/moyu-aosu-gts2m",
+        Img: "cubeslist/gts24.jpeg"
+    },
+    {
+        Name: "MoYu Aosu WR M",
+        Weight: "119.8g",
+        Size: "59mm",
+        Stickerless: "Yes",
+        Magnetic: "Yes",
+        Price: "$42.99",
+        Rating: "8/10",
+        Link: "https://speedcubing.org/collections/4x4x4/products/moyu-aosu-wrm",
+        Img: "cubeslist/aosu.jpeg"
+    },
+    {
+        Name: "MoYu RS4M",
+        Weight: "180g",
+        Size: "62mm",
+        Stickerless: "Yes",
+        Magnetic: "Yes",
+        Price: "$14.99",
+        Rating: "5/10",
+        Link: "https://speedcubing.org/collections/4x4x4/products/moyu-rs4m",
+        Img: "cubeslist/rs4m.jpeg"
+    },
+    {
+        Name: "Qiyi MS",
+        Weight: "135.6g",
+        Size: "62mm",
+        Stickerless: "Yes",
+        Magnetic: "Yes",
+        Price: "$11.99",
+        Rating: "6/10",
+        Link: "https://speedcubing.org/collections/4x4x4/products/qiyi-ms-4x4x4-black",
+        Img: "cubeslist/qiyims4"
+    },
+    {
+        Name: "Qiyi Qiyuan",
+        Weight: "148g",
+        Size: "62mm",
+        Stickerless: "Yes",
+        Magnetic: "No",
+        Price: "$7.99",
+        Rating: "5/10",
+        Link: "https://speedcubing.org/collections/4x4x4/products/qiyi-qiyuan-s2",
+        Img: "cubeslist/qiyuan.jpeg"
+    },
+    {
+        Name: "Qiyi Qiyuan S",
+        Weight: "132.6g",
+        Size: "62mm",
+        Stickerless: "Yes",
+        Magnetic: "No",
+        Price: "$7.99",
+        Rating: "5/10",
+        Link: "none",
+        Img: "cubeslist/qiyuans.jpeg"
+    },
+    {
+        Name: "Qiyi Qiyuan S V2",
+        Weight: "119g",
+        Size: "61.5mm",
+        Stickerless: "Yes",
+        Magnetic: "No",
+        Price: "$6.99",
+        Rating: "6/10",
+        Link: "https://speedcubing.org/collections/4x4x4/products/qiyi-qiyuan-s2",
+        Img: "cubeslist/qiyuansv2.jpeg"
+    },
+    {
+        Name: "Qiyi Valk",
+        Weight: "142g",
+        Size: "60mm",
+        Stickerless: "Yes",
+        Magnetic: "Yes",
+        Price: "$39.99",
+        Rating: "7/10",
+        Link: "none",
+        Img: "cubeslist/valk4.jpeg"
+    },
+    {
+        Name: "YJ YuSu",
+        Weight: "159g",
+        Size: "62.5mm",
+        Stickerless: "Yes",
+        Magnetic: "No",
+        Price: "$8.99",
+        Rating: "4/10",
+        Link: "none",
+        Img: "cubeslist/yusu.jpeg"
+    },
+    {
+        Name: "YJ YuSu V2",
+        Weight: "136g",
+        Size: "61.5mm",
+        Stickerless: "Yes",
+        Magnetic: "No",
+        Price: "$11.99",
+        Rating: "7/10",
+        Link: "https://speedcubing.org/collections/4x4x4/products/yj-yusu-v2m",
+        Img: "cubeslist/yusuv2.jpeg"
+    },
+    {
+        Name: "YJ Zhilong Mini",
+        Weight: "96g",
+        Size: "56mm",
+        Stickerless: "Yes",
+        Magnetic: "Yes",
+        Price: "$11.99",
+        Rating: "8/10",
+        Link: "https://speedcubing.org/collections/4x4x4/products/yj-zhilong-m-mini-4x4x4",
+        Img: "cubeslist/zhilong4.jpeg"
+    },
+    {
+        Name: "Yuxin Little Magic",
+        Weight: "126.3g",
+        Size: "60mm",
+        Stickerless: "Yes",
+        Magnetic: "Yes",
+        Price: "$9.99",
+        Rating: "7/10",
+        Link: "none",
+        Img: "cubeslist/lm4.jpeg"
+    },
+    {
+        Name: "X-Man Ambition M",
+        Weight: "106g",
+        Size: "60mm",
+        Stickerless: "Yes",
+        Magnetic: "Yes",
+        Price: "$29.99",
+        Rating: "9/10",
+        Link: "https://cdn.shopify.com/s/files/1/0276/0133/8446/products/01_180x.jpg?v=1619466393",
+        Img: "cubeslist/xmanambition.jpeg"
+    },
+];
